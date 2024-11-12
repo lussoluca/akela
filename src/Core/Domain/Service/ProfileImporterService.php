7fa8ba62cfa090534b1edc3c791cb7c908f8fcfb
Lorenzo <126672617+lorenzo-shots@users.noreply.github.com>
2024-11-12 23:42:07 +0100

    set OverwritableTrait.php and used in the importer data services, implement for ProfileImporterService and UnitImporterService

--- src/Core/Domain/Service/ProfileImporterService.php
+++ src/Core/Domain/Service/ProfileImporterService.php
@@ -2,20 +2,23 @@
 
 namespace App\Core\Domain\Service;
 
-use App\Core\Domain\Model\Address;
 use App\Core\Domain\Model\Email;
-use App\Core\Domain\Model\Enum\Gender;
-use App\Core\Domain\Model\Profile;
-use App\Core\Infrastructure\Persistence\Repository\ProfileRepository;
 use Symfony\Component\Uid\UuidV4;
+use App\Core\Domain\Model\Profile;
+use App\Core\Domain\Model\Address;
+use App\Core\Domain\Model\Enum\Gender;
+use App\Core\Domain\Model\Traits\OverwritableTrait;
+use App\Core\Infrastructure\Persistence\Repository\ProfileRepository;
 
 class ProfileImporterService
 {
-    public bool $overwrite = false;
+    use OverwritableTrait;
 
     public function __construct(
         private readonly ProfileRepository $profileRepository,
-    ) {}
+    )
+    {
+    }
 
     /**
      * @param array<int, array<int, bool|int|string>> $profiles
@@ -23,7 +26,7 @@ class ProfileImporterService
     public function processProfiles(array $profiles): void
     {
         foreach ($profiles as $rowData) {
-            $profile = $this->profileRepository->find((string) $rowData[0]);
+            $profile = $this->profileRepository->find((string)$rowData[0]);
             if (null !== $profile) {
                 continue;
             }
@@ -35,25 +38,64 @@ class ProfileImporterService
             }
             $birthDate = (new \DateTime())->setTimestamp($birthDate->getTimestamp());
 
-            $profile = new Profile(
-                firstname: (string) $rowData[1],
-                lastname: (string) $rowData[2],
-                birthAddress: new Address(
-                    countryCode: 'IT',
-                    administrativeArea: (string) $rowData[3],
-                    locality: (string) $rowData[4],
-                    postalCode: '',
-                    addressLine1: '',
-                    locale: 'it'
-                ),
-                birthDate: $birthDate,
-                fiscalCode: (string) $rowData[6],
-                email: (isset($rowData[9]) ? new Email((string) $rowData[9]) : new Email('')),
-                phone: (string) ($rowData[8] ?? ''),
-                gender: Gender::UNDEFINED,
-                id: new UuidV4((string) $rowData[0]),
-            );
+
+            if ($this->isOverwritable()) {
+                if (null !== $profile) {
+                    $profile->update(
+                        firstname: (string)$rowData[1],
+                        lastname: (string)$rowData[2],
+                        birthAddress: new Address(
+                            countryCode: 'IT',
+                            administrativeArea: (string)$rowData[3],
+                            locality: (string)$rowData[4],
+                            postalCode: '',
+                            addressLine1: '',
+                            locale: 'it'
+                        ),
+                        birthDate: $birthDate,
+                        fiscalCode: (string)$rowData[6],
+                        email: (isset($rowData[9]) ? new Email((string)$rowData[9]) : new Email('')),
+                        phone: (string)($rowData[8] ?? ''),
+                        gender: Gender::UNDEFINED,
+                    );
+                } else {
+                    $profile = $this->createGroup($rowData, $uuid);
+                }
+            } else {
+                if (null !== $profile) {
+                    continue;
+                }
+                $profile = $this->createProfile($rowData, $uuid);
+            }
             $this->profileRepository->add($profile);
         }
+    }
+
+    /**
+     * @param array $rowData
+     * @param \DateTime $birthDate
+     * @return array
+     */
+    public function createProfile(array $rowData, \DateTime $birthDate): array
+    {
+        $profile = new Profile(
+            firstname: (string)$rowData[1],
+            lastname: (string)$rowData[2],
+            birthAddress: new Address(
+                countryCode: 'IT',
+                administrativeArea: (string)$rowData[3],
+                locality: (string)$rowData[4],
+                postalCode: '',
+                addressLine1: '',
+                locale: 'it'
+            ),
+            birthDate: $birthDate,
+            fiscalCode: (string)$rowData[6],
+            email: (isset($rowData[9]) ? new Email((string)$rowData[9]) : new Email('')),
+            phone: (string)($rowData[8] ?? ''),
+            gender: Gender::UNDEFINED,
+            id: new UuidV4((string)$rowData[0]),
+        );
+        return $profile;
     }
 }
--- src/Core/Domain/Service/UnitImporterService.php
+++ src/Core/Domain/Service/UnitImporterService.php
@@ -4,6 +4,7 @@ namespace App\Core\Domain\Service;
 
 use App\Core\Domain\Model\Enum\UnitType;
 use App\Core\Domain\Model\GroupInterface;
+use App\Core\Domain\Model\Traits\OverwritableTrait;
 use App\Core\Domain\Model\Unit;
 use App\Core\Infrastructure\Persistence\Repository\GroupRepository;
 use App\Core\Infrastructure\Persistence\Repository\UnitRepository;
@@ -11,7 +12,7 @@ use Symfony\Component\Uid\UuidV4;
 
 class UnitImporterService
 {
-    public bool $overwrite = false;
+    use OverwritableTrait;
 
     public function __construct(
         private readonly UnitRepository $unitRepository,
@@ -29,20 +30,39 @@ class UnitImporterService
                 throw new \Exception('Unknown unit type: '.$rowData[3]);
             }
 
-            /** @var GroupInterface $group */
+            /** @var ?GroupInterface $group */
             $group = $this->groupRepository->find((string) $rowData[1]);
-            $unit = $this->unitRepository->find((string) $rowData[0]);
-            if (null !== $unit) {
-                continue;
+            if (null === $group) {
+                throw new \Exception('Unknown group with id: '.$rowData[1]);
             }
 
-            $unit = new Unit(
-                name: (string) $rowData[1],
-                type: $type,
-                group: $group,
-                id: new UuidV4((string) $rowData[0])
-            );
+            $unit = $this->unitRepository->find((string) $rowData[0]);
+            if ($this->isOverwritable()) {
+                if (null !== $unit) {
+                    $unit->update(name: (string) $rowData[2], type: $type);
+                } else {
+                    $unit = $this->createUnit($rowData, $type, $group);
+                }
+            } else {
+                if (null !== $unit) {
+                    continue;
+                }
+                $unit = $this->createUnit($rowData, $type, $group);
+            }
             $this->unitRepository->add($unit);
         }
+    }
+
+    /**
+     * @param array<int, bool|int|string> $rowData
+     */
+    public function createUnit(array $rowData, UnitType $type, GroupInterface $group): Unit
+    {
+        return new Unit(
+            name: (string) $rowData[1],
+            type: $type,
+            group: $group,
+            id: new UuidV4((string) $rowData[0])
+        );
     }
 }

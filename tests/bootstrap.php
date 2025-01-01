d80fc1a3cf0a15fd44747e3232dcb7909d5d996b
Luca Lusso <lussoluca@gmail.com>
2025-01-01 18:30:31 +0100

    #9 Setup test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,7 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+passthru('bin/console doctrine:database:drop --env=test --force');
+passthru('bin/console doctrine:database:create --env=test');
+passthru('bin/console doctrine:schema:create --env=test');
